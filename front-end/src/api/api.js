dfde5eae75269130360f3f53afb78ea9899da792
harlemsilvas <harlem.claumann@gmail.com>
2025-03-12 21:55:45 -0300

    ajustes na url do deploy/api.js

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -3,7 +3,8 @@ import "dotenv/config";
 import axios from "axios";
 
 const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://deploy-jornada-full-stack-mjks.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
